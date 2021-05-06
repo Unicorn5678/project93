ccf377a782d1beea53890e57c40029d6a8aa1941
Unicorn5678 <77387530+Unicorn5678@users.noreply.github.com>
2021-05-06 11:47:37 +0530

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyAPlw0M8LduDzT2h6qgfLiuJC_G6ecxswA",
+    authDomain: "project94-41b26.firebaseapp.com",
+    databaseURL: "https://project94-41b26-default-rtdb.firebaseio.com",
+    projectId: "project94-41b26",
+    storageBucket: "project94-41b26.appspot.com",
+    messagingSenderId: "697010781397",
+    appId: "1:697010781397:web:359695d20412dab3c7765c"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
